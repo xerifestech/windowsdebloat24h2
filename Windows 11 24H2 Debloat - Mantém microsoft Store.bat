d0c9264bb43c7ada66cc476adc75366033c39125
xerifestech <78010876+xerifestech@users.noreply.github.com>
2024-10-11 10:57:17 -0400

    Add files via upload

---
+++ Windows 11 24H2 Debloat - Mantém microsoft Store.bat
@@ -0,0 +1,28 @@
+@echo off
+:: Desativar Telemetria
+sc delete DiagTrack
+sc delete dmwappushservice
+echo "Telemetria desativada."
+
+:: Remover aplicativos pré-instalados, exceto a Microsoft Store
+powershell -Command "Get-AppxPackage -AllUsers | Where-Object { $_.Name -notlike '*Store*' } | Remove-AppxPackage"
+powershell -Command "Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -notlike '*Store*' } | Remove-AppxProvisionedPackage -Online"
+echo "Aplicativos pré-instalados removidos, exceto a Microsoft Store."
+
+:: Desativar Cortana
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v AllowCortana /t REG_DWORD /d 0 /f
+echo "Cortana desativada."
+
+:: Desativar sugestões de aplicativos
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SystemPaneSuggestionsEnabled /t REG_DWORD /d 0 /f
+echo "Sugestões de aplicativos desativadas."
+
+:: Desativar anúncios no Explorador de Arquivos
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowSyncProviderNotifications /t REG_DWORD /d 0 /f
+echo "Anúncios no Explorador de Arquivos desativados."
+
+start https://www.youtube.com/@xerifetech?sub_confirmation=1
+
+
+echo "Debloat concluído. Por favor, reinicie o computador para aplicar todas as mudanças."
+pause
